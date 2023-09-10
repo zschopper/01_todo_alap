ed3f30e4258fe94389dd2479b0f979e212a854dd
csefikatalin <csefi.katalin@gmail.com>
2023-09-10 08:19:31 +0200

    todo kiindulo fájlok 1. dolgozathoz

---
+++ adatok.js
@@ -0,0 +1,29 @@
+export const TODOLIST = [
+    "szakdolgozat specifikáció elkészítése",
+    "drótváz, funkciólista",
+    "adatbázis tervezés, létrehozás",
+    "backend 80%",
+    "frontend 80%",
+    "OSZTV",
+    "tesztelés",
+    "dokumentáció",
+    "kész szakdolgozat",
+];
+export const TODOLIST2 = [
+    {
+        tevekenyseg: "szakdolgozat specifikáció elkészítése",
+        hatarido: "2023.10.15",
+    },
+    { tevekenyseg: "drótváz, funkciólista", hatarido: "2023.10.30" },
+    { tevekenyseg: "adatbázis tervezés, létrehozás", hatarido: "2023.12.11" },
+    { tevekenyseg: "backend 80%", hatarido: "2024.01.20" },
+    { tevekenyseg: "frontend 80%", hatarido: "2023.02.11" },
+    { tevekenyseg: "OSZTV", hatarido: "2023.02.26" },
+    { tevekenyseg: "tesztelés, dokumentáció", hatarido: "folyamatosan" },
+   
+    {
+        tevekenyseg: "kész szakdolgozat legalább 99%-os készültséggel",
+        hatarido: "2024.03.31",
+    },
+    { tevekenyseg: "kész dokumentáció", hatarido: "2024.04.15" }
+];
